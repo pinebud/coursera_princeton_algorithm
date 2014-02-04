641fb1e9af01811d2972c928b2b9fb4c20109681
yuan.li <yuan.li@thomsonreuters.com>
2014-02-04 17:09:12 +0800

    Quick Find implementation, TODO:: Update UT and add performance test part

--- algorithm/src/main/java/coursera/princeton/algorithm/part1/week1/ISet.java
+++ algorithm/src/main/java/coursera/princeton/algorithm/part1/week1/ISet.java
@@ -1,8 +1,11 @@
 package coursera.princeton.algorithm.part1.week1;
 
-public interface ISet<T, S> {
-	public void init(T[] input);
-	public void union(T a, T b);
-	public boolean connected(T a, T b);
-	public S find(T a);
+/*
+ * API of union find, T is the 
+ */
+public interface ISet<T, S> {	
+    public void init(int size);
+    public void union(T a, T b);
+    public boolean connected(T a, T b);
+    public S find(T a);
 }
