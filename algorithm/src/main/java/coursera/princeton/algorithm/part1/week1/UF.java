85448fa2aef600f8a6066ab4235ee5dab66a8716
yuan.li <yuan.li@thomsonreuters.com>
2014-02-05 01:21:20 +0800

    Quick Find implementation

---
+++ algorithm/src/main/java/coursera/princeton/algorithm/part1/week1/UF.java
@@ -0,0 +1,34 @@
+package coursera.princeton.algorithm.part1.week1;
+
+public class UF implements ISet<Integer, Integer> {
+    Integer[] set = null;
+    int size = 0;
+
+    public void init(int size) {
+        this.size = size;
+        set = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            set[i] = i;
+        } 
+    }
+
+    public void union(Integer a, Integer b) {
+        Integer sa = set[a];
+        Integer sb = set[b];
+        if (sa != sb) {
+            for (int i = 0; i < size; i++) {
+                if (set[i] == sa) {
+                    set[i] = sb;
+                }
+            }
+        }
+    }    
+
+    public boolean connected(Integer a, Integer b) {
+        return set[a] == set[b];
+    }
+
+    public Integer find(Integer a) {
+        return set[a];
+    }
+}
