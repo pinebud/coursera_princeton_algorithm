0b1889075cc79e54d8fdaa394706c4db9e8d4206
jing.ning <jing.ning@zuora.com>
2014-03-06 18:27:42 +0800

    Add unit test and readjust the row and column order in function parameters

--- algorithm/src/main/java/Percolation.java
+++ algorithm/src/main/java/Percolation.java
@@ -1,138 +1,142 @@
 public class Percolation {
+    private int nsize = 0;
+    private WeightedQuickUnionUF wqu = null;
+    private int[] site1DimensionArray = null;
+    private boolean isPercolated = false;
 
-	private int nsize = 0;
+    /**
+     * O(N^2)
+     * 
+     * @param N
+     */
+    public Percolation(final int N) {
+        this.nsize = N;
+        // create N-by-N grid, with all sites blocked
+        int lengthOf1DArray = N * N;
+        wqu = new WeightedQuickUnionUF(lengthOf1DArray);
+        site1DimensionArray = new int[lengthOf1DArray];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                site1DimensionArray[j * N + i] = 0;
+            }
+        }
+    }
 
-	private WeightedQuickUnionUF wqu = null;
+    /**
+     * 
+     * @param row
+     *            : from 1 to N
+     * @param column
+     *            : from 1 to N
+     */
+    public void open(int row, int col) {
 
-	private int[] site1DimensionArray = null;
+        if (isOpen(row, col)) {
+            return;
+        }
 
-	private boolean isPercolated = false;
+        validate(row, col);
 
-	/**
-	 * O(N^2)
-	 * 
-	 * @param N
-	 */
-	public Percolation(final int N) {
-		this.nsize = N;
-		// create N-by-N grid, with all sites blocked
-		int lengthOf1DArray = N * N;
-		wqu = new WeightedQuickUnionUF(lengthOf1DArray);
-		site1DimensionArray = new int[lengthOf1DArray];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				site1DimensionArray[j * N + i] = 0;
-			}
-		}
-	}
+        int curIndex = (row - 1) * nsize + col - 1;
+        // open site (row i, column j) if it is not already
 
-	/**
-	 * 
-	 * @param i
-	 *            : from 1 to N
-	 * @param j
-	 *            : From 1 to N
-	 */
-	public void open(int i, int j) {
+        // int top = (j > 0 ? j - 1 : j) * n + i;
+        // int bottom = (j < n - 1 ? j + 1 : j) * n + i;
+        // int left = j * n + (i > 0 ? i - 1 : i);
+        // int right = j * n + (i < n - 1 ? i + 1 : i);
 
-		validate(i, j);
+        int top = (row - 2) * nsize + col - 1;
+        int bottom = (row * nsize + col - 1);
+        int left = (row - 1) * nsize + col - 2;
+        int right = (row - 1) * nsize + col;
 
-		int curIndex = (j - 1) * nsize + i - 1;
-		// open site (row i, column j) if it is not already
-		// int top = (j > 0 ? j - 1 : j) * n + i;
-		// int bottom = (j < n - 1 ? j + 1 : j) * n + i;
-		// int left = j * n + (i > 0 ? i - 1 : i);
-		// int right = j * n + (i < n - 1 ? i + 1 : i);
+        if (row > 1 && isOpen(top)) {
+            wqu.union(top, curIndex);
+        }
+        if (row < nsize && isOpen(bottom)) {
+            wqu.union(bottom, curIndex);
+        }
+        if (col > 1 && isOpen(left)) {
+            wqu.union(left, curIndex);
+        }
+        if (col < nsize && isOpen(right)) {
+            wqu.union(right, curIndex);
+        }
 
-		int top = (j - 2) * nsize + i - 1;
-		int bottom = (j * nsize + i - 1);
-		int left = (j - 1) * nsize + i - 2;
-		int right = (j - 1) * nsize + i;
+        site1DimensionArray[curIndex] = 1;
 
-		if (j > 1 && isOpen(top)) {
-			wqu.union(top, curIndex);
-		}
-		if (j < nsize && isOpen(bottom)) {
-			wqu.union(bottom, curIndex);
-		}
-		if (i > 1 && isOpen(left)) {
-			wqu.union(left, curIndex);
-		}
-		if (i < nsize && isOpen(right)) {
-			wqu.union(right, curIndex);
-		}
+        if (isFull(row, col) && isConnectedToBottom(row, col)) {
+            isPercolated = true;
+        }
+    }
 
-		site1DimensionArray[curIndex] = 1;
+    /**
+     * 
+     * @param row
+     *            : from 1 to N
+     * @param col
+     *            : from 1 to N
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        // is site (row i, column j) open?
+        validate(row, col);
 
-		if (isFull(i, j) && isConnectedToBottom(i, j)) {
-			isPercolated = true;
-		}
-	}
+        return site1DimensionArray[(row - 1) * nsize + col - 1] == 1;
+    }
 
-	/**
-	 * 
-	 * @param i
-	 *            : from 1 to N
-	 * @param j
-	 *            : from 1 to N
-	 * @return
-	 */
-	public boolean isOpen(int i, int j) {
-		// is site (row i, column j) open?
-		validate(i, j);
+    private boolean isOpen(int index) {
+        return site1DimensionArray[index] == 1;
+    }
 
-		return site1DimensionArray[(j - 1) * nsize + i - 1] == 1;
-	}
+    private boolean isConnectedToBottom(int row, int col) {
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        if (row == nsize) {
+            return true;
+        }
+        int curIndex = (row - 1) * nsize + col - 1;
+        int base = nsize * nsize - nsize;
+        for (int k = 0; k < nsize; k++) {
+            if (wqu.connected(curIndex, base + k)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private boolean isOpen(int index) {
-		return site1DimensionArray[index] == 1;
-	}
+    /*
+     * O(N)
+     */
+    public boolean isFull(int row, int col) {
 
-	private boolean isConnectedToBottom(int i, int j) {
-		if (!isOpen(i, j)) {
-			return false;
-		}
-		if (j == nsize) {
-			return true;
-		}
-		int curIndex = (j - 1) * nsize + i - 1;
-		int base = nsize * nsize - nsize;
-		for (int k = 0; k < nsize; k++) {
-			if (wqu.connected(curIndex, base + k)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        validate(row, col);
 
-	/*
-	 * O(N)
-	 */
-	public boolean isFull(int i, int j) {
-		// is site (row i, column j) full?
-		if (!isOpen(i, j)) {
-			return false;
-		}
-		if (j == 1) {
-			return true;
-		}
-		int curIndex = (j - 1) * nsize + i - 1;
-		for (int k = 0; k < nsize; k++) {
-			if (wqu.connected(curIndex, k)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        // is site (row i, column j) full?
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        if (row == 1) {
+            return true;
+        }
+        int curIndex = (row - 1) * nsize + col - 1;
+        for (int k = 0; k < nsize; k++) {
+            if (wqu.connected(curIndex, k)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public boolean percolates() {
-		// does the system percolate?
-		return isPercolated;
-	}
+    public boolean percolates() {
+        // does the system percolate?
+        return isPercolated;
+    }
 
-	private void validate(int i, int j) {
-		if (i < 1 || i > nsize || j < 1 || j > nsize)
-			throw new IndexOutOfBoundsException();
-	}
+    private void validate(int row, int col) {
+        if (row < 1 || row > nsize || col < 1 || col > nsize)
+            throw new IndexOutOfBoundsException();
+    }
 
 }
--- algorithm/src/main/java/PercolationStats.java
+++ algorithm/src/main/java/PercolationStats.java
@@ -1,69 +1,68 @@
 public class PercolationStats {
+    private double[] results = null;
 
-	private double[] results = null;
+    private double mean = 0.0d;
 
-	private double mean = 0.0d;
+    private double stddev = 0.0d;
 
-	private double stddev = 0.0d;
+    private double conflo = 0.0d;
 
-	private double conflo = 0.0d;
+    private double confhi = 0.0d;
 
-	private double confhi = 0.0d;
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0)
+            throw new java.lang.IllegalArgumentException();
+        results = new double[T];
+        double totalSites = N * N;
+        // perform T independent computational experiments on an N-by-N grid
+        for (int t = 0; t < T; t++) {
+            long cntOfOpenSites = 0;
+            Percolation perc = new Percolation(N);
+            while (!perc.percolates()) {
+                int rdmI = StdRandom.uniform(N) + 1;
+                int rdmJ = StdRandom.uniform(N) + 1;
+                if (!perc.isOpen(rdmI, rdmJ)) {
+                    perc.open(rdmI, rdmJ);
+                    cntOfOpenSites++;
+                }
+            }
+            results[t] = (double) cntOfOpenSites / totalSites;
+        }
+        mean = StdStats.mean(results);
+        stddev = StdStats.stddev(results);
+        conflo = mean - 1.96 * stddev / Math.sqrt(T);
+        confhi = mean + 1.96 * stddev / Math.sqrt(T);
+    }
 
-	public PercolationStats(int N, int T) {
-		if (N <= 0 || T <= 0)
-			throw new java.lang.IllegalArgumentException();
-		results = new double[T];
-		double totalSites = N * N;
-		// perform T independent computational experiments on an N-by-N grid
-		for (int t = 0; t < T; t++) {
-			long cntOfOpenSites = 0;
-			Percolation perc = new Percolation(N);
-			while (!perc.percolates()) {
-				int rdmI = StdRandom.uniform(N) + 1;
-				int rdmJ = StdRandom.uniform(N) + 1;
-				if (!perc.isOpen(rdmI, rdmJ)) {
-					perc.open(rdmI, rdmJ);
-					cntOfOpenSites++;
-				}
-			}
-			results[t] = (double) cntOfOpenSites / totalSites;
-		}
-		mean = StdStats.mean(results);
-		stddev = StdStats.stddev(results);
-		conflo = mean - 1.96 * stddev / Math.sqrt(T);
-		confhi = mean + 1.96 * stddev / Math.sqrt(T);
-	}
+    public double mean() {
+        // sample mean of percolation threshold
+        return mean;
+    }
 
-	public double mean() {
-		// sample mean of percolation threshold
-		return mean;
-	}
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        return stddev;
+    }
 
-	public double stddev() {
-		// sample standard deviation of percolation threshold
-		return stddev;
-	}
+    public double confidenceLo() {
+        // returns lower bound of the 95% confidence interval
+        return conflo;
+    }
 
-	public double confidenceLo() {
-		// returns lower bound of the 95% confidence interval
-		return conflo;
-	}
+    public double confidenceHi() {
+        // returns upper bound of the 95% confidence interval
+        return confhi;
+    }
 
-	public double confidenceHi() {
-		// returns upper bound of the 95% confidence interval
-		return confhi;
-	}
-
-	public static void main(String[] args) {
-		// test client, described below
-		int N = Integer.valueOf(args[0]);
-		int T = Integer.valueOf(args[1]);
-		// PercolationStats instance = new PercolationStats(N, T);
-		PercolationStats instance = new PercolationStats(200, 100);
-		StdOut.println("mean\t=\t" + instance.mean());
-		StdOut.println("stddev\t=\t" + instance.stddev());
-		StdOut.println("95% confidence interval\t=\t" + instance.confidenceLo()
-				+ "," + instance.confidenceHi());
-	}
+    public static void main(String[] args) {
+        // test client, described below
+        int N = Integer.valueOf(args[0]);
+        int T = Integer.valueOf(args[1]);
+        PercolationStats instance = new PercolationStats(N, T);
+        // PercolationStats instance = new PercolationStats(200, 100);
+        StdOut.println("mean\t=\t" + instance.mean());
+        StdOut.println("stddev\t=\t" + instance.stddev());
+        StdOut.println("95% confidence interval\t=\t" + instance.confidenceLo()
+                + "," + instance.confidenceHi());
+    }
 }
